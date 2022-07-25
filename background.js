426ab143bea56f4e6183928fb4b0d7a1f5d78866
codingwithtashi <khakontas33@gmail.com>
2022-07-25 22:06:05 +0530

    store json data in indexdb

--- background.js
+++ background.js
@@ -18,3 +18,41 @@ chrome.runtime.onConnect.addListener(function (port) {
     }
   });
 });
+
+chrome.runtime.onInstalled.addListener(async function (details) {
+  if (details.reason == "install") {
+    //call a function to handle a first install
+    console.log("install for first time");
+    var response = await fetch(chrome.runtime.getURL("assets/dictionary.json"));
+    var str = await response.text();
+    var data = JSON.parse(str);
+    var idb = await importIDB("dictionary", "fstore", data);
+  } else if (details.reason == "update") {
+    //call a function to handle an update
+  }
+});
+
+function importIDB(dname, sname, wordArray) {
+  return new Promise(function (resolve) {
+    var db = indexedDB.open(dname);
+    db.onupgradeneeded = function () {
+      var idb = db.result;
+      var store = idb.createObjectStore(sname, {
+        keyPath: "name",
+        autoIncrement: true,
+      });
+    };
+    db.onsuccess = function () {
+      var idb = db.result;
+      let tactn = idb.transaction(sname, "readwrite");
+      var store = tactn.objectStore(sname);
+      for (var word of wordArray) {
+        store.put(word);
+      }
+      resolve(idb);
+    };
+    db.onerror = function (e) {
+      console.log(e);
+    };
+  });
+}
