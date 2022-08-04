549897cb17111c40c73be9be129ff6298e3937e9
codingwithtashi <khakontas33@gmail.com>
2022-08-04 08:50:08 +0530

    added enter to search word

--- popup/popup.js
+++ popup/popup.js
@@ -1,40 +1,46 @@
-//Used Port\Long Lived Connection for communicating with background page for fetching results
+document.addEventListener("DOMContentLoaded", function (event) {
+  //Used Port\Long Lived Connection for communicating with background page for fetching results
+  var port = chrome.runtime.connect({
+    name: "Sample Communication",
+  });
+  port.postMessage("Request Modified Value");
 
-var port = chrome.runtime.connect({
-  name: "Sample Communication",
-});
-port.postMessage("Request Modified Value");
+  port.onMessage.addListener(function (msg) {
+    if (msg != null && msg.method != undefined) {
+      switch (msg.method) {
+        case "wordMeaning":
+          document.getElementById("dictionary_word_defination").innerHTML =
+            msg.data.defination;
+          document.getElementById("dictionary_word_wylie").innerHTML =
+            msg.data.wylie;
+          document.getElementById("defination_div").style.display = "block";
 
-port.onMessage.addListener(function (msg) {
-  if (msg != null && msg.method != undefined) {
-    switch (msg.method) {
-      case "wordMeaning":
-        document.getElementById("dictionary_word_defination").innerHTML =
-          msg.data.defination;
-        document.getElementById("dictionary_word_wylie").innerHTML =
-          msg.data.wylie;
-        document.getElementById("defination_div").style.display = "block";
+          break;
+        default:
+          break;
+      }
+    }
+  });
 
-        break;
-      default:
-        break;
+  //
+  document
+    .getElementById("search_word_input_btn")
+    .addEventListener("click", getWordMeaning);
+  document
+    .getElementById("search_word_input")
+    .addEventListener("keypress", function (e) {
+      if (e.key === "Enter") getWordMeaning();
+    });
+
+  function getWordMeaning() {
+    var enterText = document.getElementById("search_word_input").value;
+    if (enterText) {
+      let firstWord = enterText.split(" ")[0];
+      var data = {
+        method: "searchWord",
+        data: firstWord,
+      };
+      port.postMessage(data);
     }
   }
 });
-
-//
-document
-  .getElementById("search_word_input_btn")
-  .addEventListener("click", getWordMeaning);
-
-function getWordMeaning() {
-  var enterText = document.getElementById("search_word_input").value;
-  if (enterText) {
-    let firstWord = enterText.split(" ")[0];
-    var data = {
-      method: "searchWord",
-      data: firstWord,
-    };
-    port.postMessage(data);
-  }
-}
